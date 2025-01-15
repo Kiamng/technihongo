e3eca6cfe24fd34aee6d482bb79c07ed0b5b2c1b
Kiamnq <lamnq.dev@gmail.com>
2025-01-15 15:43:55 +0700

    Add light/dark theme, add env

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import ThemeToggleButton from "@/components/core/common/theme-toggle-button";
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div>
+      <ThemeToggleButton />
       <Button>Hello world</Button>
     </div>
   );
