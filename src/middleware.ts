9ab62b53f16144fc82ca9cdd0e48df91ed996ef3
Kiamnq <lamnq.dev@gmail.com>
2025-05-07 10:30:08 +0700

    [FIX]_fix next_auth configs, fix middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,5 @@
 import type { NextRequest } from "next/server";
 
-import { getToken } from "next-auth/jwt";
 import { NextResponse } from "next/server";
 
 const publicRoutes = ["/sign-in", "/Login", "/verify"];
@@ -21,35 +20,43 @@ const authRoutes = [
 ];
 
 export default async function middleware(req: NextRequest) {
-  const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
-  const { pathname } = req.nextUrl;
+  try {
+    console.log("Request Headers:", req.headers);
+    // const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+    const token = req.cookies.get("__Secure-next-auth.session-token");
+    const { pathname } = req.nextUrl;
 
-  console.log("Path:", pathname, "Token:", token);
+    console.log("Path:", pathname, "Token:", token);
 
-  const isPublicRoute = publicRoutes.includes(pathname);
-  const isAuthRoute = authRoutes.some((route) => pathname.startsWith(route));
+    const isPublicRoute = publicRoutes.includes(pathname);
+    const isAuthRoute = authRoutes.some((route) => pathname.startsWith(route));
 
-  // Nếu user đã đăng nhập và đang cố vào trang public -> chuyển hướng về /home
-  if (token && isPublicRoute && pathname !== "/course") {
-    return NextResponse.redirect(new URL("/course", req.url));
-  }
+    // Nếu user đã đăng nhập và đang cố vào trang public -> chuyển hướng về /home
+    if (token && isPublicRoute && pathname !== "/course") {
+      return NextResponse.redirect(new URL("/course", req.url));
+    }
 
-  // Nếu user chưa đăng nhập và cố vào trang yêu cầu auth -> chuyển hướng về /login
-  if (!token && isAuthRoute && pathname !== "/Login") {
-    return NextResponse.redirect(new URL("/Login", req.url));
-  }
+    // Nếu user chưa đăng nhập và cố vào trang yêu cầu auth -> chuyển hướng về /login
+    if (!token && isAuthRoute && pathname !== "/Login") {
+      return NextResponse.redirect(new URL("/Login", req.url));
+    }
 
-  if (pathname === "/") {
+    if (pathname === "/") {
+      return NextResponse.next();
+    }
+
+    if (!isPublicRoute && !isAuthRoute) {
+      return NextResponse.redirect(new URL("/not_found", req.url));
+    }
+
     return NextResponse.next();
-  }
+  } catch (error) {
+    console.error("Middleware error:", error);
 
-  if (!isPublicRoute && !isAuthRoute) {
     return NextResponse.redirect(new URL("/not_found", req.url));
   }
-
-  return NextResponse.next();
 }
 
 export const config = {
-  matcher: ["/((?!api|_next|static|favicon.ico|not_found).*)"], // Loại trừ các route không cần middleware xử lý
+  matcher: ["/((?!api|_next|static|favicon.ico|assets|media|not_found).*)"], // Loại trừ các route không cần middleware xử lý
 };
